2cdf37e8b663ad22631443989226d402a4865b7c
shubhdeep <shubhdeepmaan5911@gmail.com>
2026-06-21 15:36:45 +0530

    add allowed host for render

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='127.0.0.1,localhost'
+).split(',')
 
 
 # Application definition
